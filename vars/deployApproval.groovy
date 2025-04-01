50bdc460e8ed16d321da8d746d6128cdf86945c0
Anuj yadav <anujpbh2022@gmail.com>
2025-04-01 13:12:16 +0530

    Update deployApproval.groovy

--- vars/deployApproval.groovy
+++ vars/deployApproval.groovy
@@ -1,1 +1,17 @@
 
+def call() {
+    def userInput = input(
+        message: "Do you approve the deployment of the postgres role?",
+        parameters: [
+            choice(name: 'Approval', choices: ['Approve', 'Reject'], description: 'Approve or Reject the deployment')
+        ]
+    )
+
+    if (userInput == 'Approve') {
+        echo "Deployment can be done"
+    } else {
+        echo "Deployment is rejected"
+        error("Deployment has been rejected. Stopping pipeline.")
+    }
+}
+
