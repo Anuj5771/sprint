19f15a838242a700da89d9ac8ed108d284977f42
Anuj yadav <anujpbh2022@gmail.com>
2025-04-01 15:05:02 +0530

    Update gitFunctions.groovy

--- vars/gitFunctions.groovy
+++ vars/gitFunctions.groovy
@@ -1,24 +1,26 @@
-def getVersionAndTag() {
-    def VERSION = '1.0.0'  
-    def BUILD_NUM = env.BUILD_NUMBER ?: 'local'  
-    def TAG_NAME = "v${VERSION}-build${BUILD_NUM}" 
-    return [VERSION: VERSION, TAG_NAME: TAG_NAME]
-}
-
 def commitAndPushChanges(String branch) {
     def result = getVersionAndTag()  
     def VERSION = result.VERSION
     def TAG_NAME = result.TAG_NAME
 
-    sh """
-        git add .
-        git commit -m "Automated commit - ${TAG_NAME}"
-        git tag ${TAG_NAME}  # Tagging with version
-        git push origin ${branch}  # Branch push
-        git push origin ${TAG_NAME}  # Tag push
-        echo "Version: ${VERSION}, Tag: ${TAG_NAME}"  # Version aur Tag dikhana
-    """
+    
+    def changes = sh(script: "git status --porcelain", returnStdout: true).trim()
+
+    if (changes) {
+        // If there are changes, commit and push
+        sh """
+            git add .
+            git commit -m "Automated commit - ${TAG_NAME}"
+            git tag ${TAG_NAME}  # Tagging with version
+            git push origin ${branch}  # Branch push
+            git push origin ${TAG_NAME}  # Tag push
+            echo "Version: ${VERSION}, Tag: ${TAG_NAME}"  # Version and Tag info
+        """
+    } else {
+        
+        echo "No changes to commit"
+    }
 }
 
-// Call function with branch name
+
 commitAndPushChanges('main')
