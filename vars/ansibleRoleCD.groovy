156cb84eb0bd0c26f6164fb216164419250f0bd8
Anuj yadav <anujpbh2022@gmail.com>
2025-04-01 13:11:53 +0530

    Update ansibleRoleCD.groovy

--- vars/ansibleRoleCD.groovy
+++ vars/ansibleRoleCD.groovy
@@ -1,1 +1,7 @@
 
+def call(String url, String branch, String creds, String parentDir, String playbookToRun) {
+    gitCheckout(url, branch, creds)
+    deployApproval()
+    runPlaybook(parentDir, playbookToRun)
+}
+
