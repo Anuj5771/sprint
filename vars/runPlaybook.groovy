77dd0d946e0d3bc6a73fe848e7deb7a469cff3ca
Anuj yadav <anujpbh2022@gmail.com>
2025-04-01 13:12:34 +0530

    Update runPlaybook.groovy

--- vars/runPlaybook.groovy
+++ vars/runPlaybook.groovy
@@ -1,1 +1,15 @@
 
+def call(String parentDir, String playbookToRun) {
+    stage("Run playbook") {
+        script {
+            def dirPath = parentDir ?: '.'
+
+            dir(dirPath) {
+                sh """
+                    ansible-playbook -i aws_ec2.yml ${playbookToRun}
+                """
+            }
+        }
+    }
+}
+
